b27603965175a6e6b4096993f7941eb31d3afa66
tth0m4s-code <61409295+tth0m4s-code@users.noreply.github.com>
2020-02-26 09:06:33 +0100

    Update starship.ino

--- starship.ino
+++ starship.ino
@@ -1,12 +1,12 @@
 /*
-
+ 
 */
 int switchState = 0;
-void setup() {
+void setup(){
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
-  pinMode(2,OUTPUT);
+  pinMode(2,INPUT);
 }
 
 void loop(){
@@ -20,9 +20,9 @@ else {
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
   digitalWrite(5, HIGH);
-  delay(250);
+  delay(150);
   digitalWrite(4, HIGH);
   digitalWrite(5, LOW);
-  delay(250);
+  delay(150);
  }
 }
